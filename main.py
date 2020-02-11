4e7a7566e577b2b7cd0ba09c2251d47b8f2fd9b5
rv3392 <richalverma00@gmail.com>
2020-02-11 10:57:12 +1000

    Refactoring to cleanup file structure and magic numbers.

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ from matplotlib import pyplot
 import string
 import re
 
-TRAINING_DATA = "train.csv"
-TESTING_DATA = "test.csv"
+TRAINING_DATA = "data/train.csv"
+TESTING_DATA = "data/test.csv"
 
-EPOCHS = 5
+MODEL_NAME = "model/model.h5"
+
+EPOCHS = 100
+BATCH_SIZE = 512
+VALIDATION_SPLIT = 0.1
 
 def load_data():
     train = pandas.read_csv(TRAINING_DATA)
@@ -61,7 +65,7 @@ def preprocess(train_dataframe, test_dataframe):
     print(vectorizer.vocabulary_)
 
     train_matrix = train_matrix_text.todense() #numpy.concatenate((train_matrix_text.todense(), train_matrix_location.todense()), axis = 1)
-    test_matrix = train_matrix_text.todense() #numpy.concatenate((test_matrix_text.todense(), test_matrix_location.todense()), axis = 1)
+    test_matrix = test_matrix_text.todense() #numpy.concatenate((test_matrix_text.todense(), test_matrix_location.todense()), axis = 1)
 
     test_out = train_matrix.sum(axis=0).tolist()[0]
     test_out.sort(reverse = True)
@@ -83,19 +87,22 @@ def train(data, targets):
     print(data.shape)
     print(targets.shape)
 
-    optimiser = optimizers.Adam()
+    optimiser = optimizers.Adam(lr = 0.001)
     training_model = models.Sequential()
 
-    training_model.add(layers.LSTM(128, return_sequences = True))
-    training_model.add(layers.LSTM(128))
-    training_model.add(layers.Dense(256, activation = 'relu'))
-    training_model.add(layers.Dense(256, activation = 'relu'))
+    training_model.add(layers.LSTM(1, return_sequences = True))
+    training_model.add(layers.LSTM(1, return_sequences = True))
+    training_model.add(layers.LSTM(1, return_sequences = True))
+    training_model.add(layers.LSTM(1, return_sequences = True))
+    training_model.add(layers.LSTM(1))
     training_model.add(layers.Dense(1, activation = 'sigmoid'))
     training_model.compile(loss = 'binary_crossentropy', optimizer = optimiser, 
-            metrics = ['accuracy'])
+        metrics = ['accuracy'])
 
     training_history = training_model.fit(data, targets, epochs = EPOCHS, 
-            validation_split = 0.2, batch_size = 512)
+            validation_split = VALIDATION_SPLIT, batch_size = BATCH_SIZE)
+
+    training_model.save(MODEL_NAME)
 
     return training_model, training_history
 
@@ -115,8 +122,11 @@ def plot_diagnostics(history):
 
 train_dataframe, test_dataframe = load_data()
 
+print(train_dataframe)
+print(test_dataframe)
+
 train_dataframe = train_dataframe.sample(frac=1).reset_index(drop=True)
-test_dataframe = test_dataframe.sample(frac=1).reset_index(drop=True)
+#test_dataframe = test_dataframe.sample(frac=1).reset_index(drop=True)
 
 train_matrix, test_matrix = preprocess(train_dataframe, test_dataframe)
 
@@ -124,8 +134,11 @@ model, training_history = train(train_matrix, numpy.asmatrix(train_dataframe['ta
 plot_diagnostics(training_history)
 
 results = test(model, test_matrix)
+results = results.round(0)
+results = results.astype(int)
 
 sample = pandas.read_csv("sample_submission.csv")
+
 sample['target'] = results
 sample.to_csv("submission.csv", index = False)
 
